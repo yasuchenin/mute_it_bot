c40e98b6f6046fe51340ba6f66df3ed5a2e81fa5
pavel <p.yasyuchenin@dats.team>
2022-10-17 01:00:10 +0300

    first commit

---
+++ src/main/java/com/yasuchenin/muteitbot/MuteItBotApplication.java
@@ -0,0 +1,13 @@
+package com.yasuchenin.muteitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuteItBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuteItBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasuchenin/muteitbot/MuteItBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasuchenin.muteitbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuteItBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
